f1aacf5fd61637a573b51fe4ec5723168d08d406
bagchitech <bagchitechscribbles@gmail.com>
2026-07-07 09:11:18 +0530

    datastructures implemented

--- include/sensors.h
+++ include/sensors.h
@@ -4,4 +4,42 @@
 #define SENSORS_H
 
 #include<stdio.h>
+#include<stdint.h>
+typedef enum {
+    ACTIVE,
+    PASSIVE,
+    ERROR
+}Status;
+
+typedef enum {
+    Temperature,
+    Humidity,
+    Pressure
+}SensorType;
+
+typedef union{
+    struct{
+        short int min_range;
+        short int max_range;
+        float reading;
+    }temperature;
+
+    struct {
+        float calibration;
+        float reading;
+    }humidity;
+
+    struct {
+        float altitude;
+        float reading;
+    }pressure;
+}DataConfig;
+
+typedef struct{
+    uint8_t id;
+    char name[30];
+    DataConfig dataConfig;
+    SensorType sensorType;
+    Status status;
+}Sensor;
 #endif //SENSORS_H
