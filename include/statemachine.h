ec240e251f125dfebc83582419f18a0cf9df456f
bagchitech <bagchitechscribbles@gmail.com>
2026-07-13 18:06:50 +0530

    state machine implemented (#13)

---
+++ include/statemachine.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef STATEMACHINE_H
+#define STATEMACHINE_H
+
+#include<stdbool.h>
+#include<stdint.h>
+#include"sensors.h"
+
+#define ERROR_THRESHOLD 5 
+#define RECOVERY_THRESHOLD 3
+
+/*checks type-specific bounds*/
+bool is_reading_valid(const Sensor *s);
+
+/*Apply the transition rules*/
+void update_sensor_state(Sensor *s);
+
+#endif //STATEMACHINE_H
