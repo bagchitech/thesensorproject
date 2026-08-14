7929eb81738316e88b4a6ccd50c59f733d25ec06
bagchitech <bagchitechscribbles@gmail.com>
2026-08-14 11:06:23 +0530

    added file parser

---
+++ include/config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "sensors.h"
+
+#define MAX_LINE_LENGTH 256
+
+Sensor *load_sensor_config(const char *path, size_t *out_count);
+static int parse_sensor_line(char *line, Sensor *out, const char *path, int line_number);
+
+#endif //SENSOR_H
--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include "sensors.h"
 #include<signal.h>
 #include<windows.h>
+#include "config.h"
 
 #define SENSOR_COUNT 21
 
-
+char path[] = "";
 static volatile sig_atomic_t running = 1;
 
 static void handle_sigint(int sig) {
@@ -29,15 +30,45 @@ int main(){
     size_t count = ARRAY_LENGTH(sensors);
 
     print_all_sensors(sensors, count);
+        FILE *fptr;
+    fptr = fopen(path, "r");
+    char line[100];
 
-    while (running) {  
-    for (size_t i = 0; i < count; i++) {
-        tick_sensor(&sensors[i]);
+    while(fgets(line, sizeof(line),fptr)!=NULL){
+        if (line[0] == '#') continue;              // it's a comment
+        if (line[0] == '\n') continue;             // it's blank
+        if (line[0] == '\0') continue;             // also blank
+        if(strncmp(line, "sensor",6)==0){
+            char *p = line;
+            p +=6; //to skip sensor
+            while (*p == ' ') p++;
+            char *end;
+            long id = strtol(p, &end, 10);
+            p = end;
+    char name[30];
+    if (*p == '"') {
+        p++;
+        char *end_quote = strchr(p, '"');
+        size_t len = end_quote - p;
+        memcpy(name, p, len);
+        name[len] = '\0';
+        p = end_quote + 1;
     }
-    print_all_sensors(sensors, count);
-    printf("---\n");
-    Sleep(10000);
-}
+            printf("%s", line); // Prints each line
+        }
+     
+    }
+
+    fclose(fptr);
+
+//     while (running) {  
+//     for (size_t i = 0; i < count; i++) {
+//         tick_sensor(&sensors[i]);
+//     }
+//     print_all_sensors(sensors, count);
+//     printf("---\n");
+//     Sleep(10000);
+// }
 
 
     // printf("Printing some values to confirm flow");
